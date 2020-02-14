0cccb8fe091692f19daded5f633850ebe47a33a2
cychen-china <59906361+cychen-china@users.noreply.github.com>
2020-02-14 18:22:46 +0800

    Add files via upload

---
+++ 陈慈媛+P3/mvn_pra/src/main/java/helloworld.java
@@ -0,0 +1,11 @@
+package helloworld;
+public class helloworld {
+    public String sayHello() {
+        return "Hello Maven!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new HelloWorld().sayHello());
+    }
+
+}
---
+++ 陈慈媛+P3/mvn_pra/src/test/java/helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld { public String sayHello() {
+    return "Hello Maven!";
+}
+
+    public static void main(String[] args) {
+        System.out.println(new HelloWorld().sayHello());
+
+}
