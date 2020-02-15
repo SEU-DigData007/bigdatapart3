431aafd0ab9df95029c10ef1cf4f63ea3bdaaa48
dhj0506 <dhj0506@gmail.com>
2020-02-15 15:53:52 +0800

    提交作业

---
+++ 丁宏剑+part3/demo1/src/main/java/com/example/demo1/sys/mapper/FooMapper.java
@@ -0,0 +1,16 @@
+package com.example.demo1.sys.mapper;
+
+import com.example.demo1.sys.entity.Foo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author ${author}
+ * @since 2020-02-04
+ */
+public interface FooMapper extends BaseMapper<Foo> {
+
+}
